ed6d22c4aa10436bed59a8aa5137a7e413e9c51c
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-03-15 13:18:56 +1300

    8bb clarified amiclk as given in TECH.DOC is correct

--- f3m.c
+++ f3m.c
@@ -113,7 +113,7 @@ typedef struct player
 	vchn_s vchn[16];
 } player_s;
 
-const uint32_t period_amiclk = 8363*1712;
+const uint32_t period_amiclk = 8363*1712-400;
 const uint16_t period_table[12] = {1712,1616,1524,1440,1356,1280,1208,1140,1076,1016,960,907};
 
 #ifdef F3M_ENABLE_DYNALOAD
